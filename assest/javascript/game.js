d6fc562e8454dd6959b49466491b5df6ff655301
Kfriedman <Koryf91@gmail.com>
2019-11-02 00:43:53 -0700

    got the buttons working

--- assest/javascript/game.js
+++ assest/javascript/game.js
@@ -1,1 +1,36 @@
 console.log("This is working just fine?");
+
+var startBtn = "";
+var userScr = 0;
+var totalScore = 0;
+var matchNum = 0;
+
+$(document).ready(function () {
+    $("#startButton").on("click", function () {
+        var ranNum = [];
+        for (var i = 0; i < 3; i++) {
+            var random = Math.floor(Math.random() *3);
+            ranNum.push(random);
+        }
+        $("#randNum").prepend("<br><hr>" + ranNum.join(""));
+    });
+});
+
+function totalScore(){
+    userScr += 1
+    return userScr;
+};
+$(document).ready(function(){
+    $(userScr).click(function(){
+        var getIncreaseValue = increment();
+        $("#userTotal").append(getIncreaseValue)
+    });
+});
+function crystal (){
+}
+    $(document).ready(function () {
+        $(".cImg").on("click", function () {
+    console.log("this :")
+
+});
+});
